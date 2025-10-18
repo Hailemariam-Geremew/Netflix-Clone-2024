cbde0309fe66d2cccf31d2bab81b5f8db93e7ed6
Hailemariam-Geremew <hailemariamgeremew828@gmail.com>
2025-10-19 01:08:28 +0300

    third time commit

--- netflix-clone/src/components/Footer/Footer.js
+++ netflix-clone/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import "./Footer.css";
+import "./footer.css";
 function Footer() {
   return (
     <div className="footer_outer_container">
